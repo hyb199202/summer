a6c867e20d0197668d331a93c078bae2b1664653
heyabin <13203971860@139.com>
2020-12-15 17:48:02 +0800

    flush

---
+++ src/main/java/com/summer/common/Constant.java
@@ -0,0 +1,11 @@
+package com.summer.common;
+
+/**
+ * @packageName: summer
+ * @author: hyb
+ * @date: 2020-12-15 下午 4:43
+ * @description:
+ */
+public class Constant {
+
+}
---
+++ src/main/java/com/summer/util/JsonUtil.java
@@ -0,0 +1,107 @@
+package com.summer.util;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.*;
+
+public class JsonUtil {
+    /**
+     * 生成.json格式文件
+     */
+    public static boolean createJsonFile(String jsonString, String filePath, String fileName) {
+        // 标记文件生成是否成功
+        boolean flag = true;
+
+        // 拼接文件完整路径
+        String fullPath = filePath + File.separator + fileName + ".json";
+
+        // 生成json格式文件
+        try {
+            // 保证创建一个新文件
+            File file = new File(fullPath);
+            if (!file.getParentFile().exists()) { // 如果父目录不存在，创建父目录
+                file.getParentFile().mkdirs();
+            }
+            if (file.exists()) { // 如果已存在,删除旧文件
+                file.delete();
+            }
+            file.createNewFile();
+
+            // 格式化json字符串
+            //jsonString = JsonFormatTool.formatJson(jsonString);
+
+            // 将格式化后的字符串写入文件
+            Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            write.write(jsonString);
+            write.flush();
+            write.close();
+        } catch (Exception e) {
+            flag = false;
+            e.printStackTrace();
+        }
+        // 返回是否成功的标记
+        return flag;
+    }
+
+
+
+    /**
+     * 读取json文件，返回json串
+     * @param fileName
+     * @return
+     */
+    public static String readJsonFile(String fileName) {
+        String jsonStr = "";
+        try {
+            File jsonFile = new File(fileName);
+            FileReader fileReader = new FileReader(jsonFile);
+
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+            int ch = 0;
+            StringBuffer sb = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                sb.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            jsonStr = sb.toString();
+            return jsonStr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+ /*   public static <T> T readJsonFromClassPath(String path, Type type) throws IOException {
+
+        ClassPathResource resource = new ClassPathResource(path);
+        if (resource.exists()) {
+            return JSON.parseObject(resource.getInputStream(), StandardCharsets.UTF_8, type,
+                    // 自动关闭流
+                    Feature.AutoCloseSource,
+                    // 允许注释
+                    Feature.AllowComment,
+                    // 允许单引号
+                    Feature.AllowSingleQuotes,
+                    // 使用 Big decimal
+                    Feature.UseBigDecimal);
+        } else {
+            throw new IOException();
+        }
+    }*/
+
+    public static boolean isJSONValid(String test) {
+        try {
+            JSONObject.parseObject(test);
+        } catch (JSONException ex) {
+            try {
+                JSONObject.parseArray(test);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
